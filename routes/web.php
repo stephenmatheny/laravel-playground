3d9a4e0173df6fd7ac6b3d048e32894e980b6e10
Stephen <smatheny@ncscredit.com>
2024-05-26 18:34:34 -0500

    complete day 13: Lazy Loading vs Eager Loading

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,12 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function (){
-    return view('jobs', ['jobs' => Job::all()]);
+    // eager loading
+    $jobs = Job::with('employer')->get();
+    // lazy loading
+    // $jobs = Job::all();
+
+    return view('jobs', ['jobs' => $jobs]);
 });
 
 Route::get('/jobs/{id}', function ($id){
