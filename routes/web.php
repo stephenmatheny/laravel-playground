9154a9f6eb217c8dfc26c91cfb6b6e36fb84a4cf
Stephen <smatheny@ncscredit.com>
2024-05-26 21:25:53 -0500

    day 16 complete

--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,35 @@ Route::get('/jobs', function (){
     // paginate
     // $jobs = Job::with('employer')->paginate(10);
     // simple paginate
-    $jobs = Job::with('employer')->simplePaginate(10);
+    $jobs = Job::with('employer')->latest()->simplePaginate(10);
     // cursor paginate
     // $jobs = Job::with('employer')->cursorPaginate(10);
 
-    return view('jobs', ['jobs' => $jobs]);
+    return view('jobs.index', ['jobs' => $jobs]);
+});
+
+Route::get('/jobs/create', function (){
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id){
     $job = Job::find(($id));
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
+});
+
+Route::post('/jobs', function (){
+    // ... TODO: validation
+    // get all of the inputs
+    // dd(request()->all());
+    // get specific imput
+    // dd(request('title'));
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1
+    ]);
+
+    return redirect('/jobs');
 });
 
 Route::get('/contact', function () {
