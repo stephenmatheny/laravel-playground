df30f0f04db18fc83bf3f5af236be5079b509326
Stephen <smatheny@ncscredit.com>
2024-05-28 16:06:59 -0500

    CRUD - create, read, update, delete

--- routes/web.php
+++ routes/web.php
@@ -8,22 +8,26 @@ Route::get('/', function () {
     return view('home');
 });
 
+// Index
 Route::get('/jobs', function (){
     $jobs = Job::with('employer')->latest()->simplePaginate(10);
 
     return view('jobs.index', ['jobs' => $jobs]);
 });
 
+// Create
 Route::get('/jobs/create', function (){
     return view('jobs.create');
 });
 
+// Show
 Route::get('/jobs/{id}', function ($id){
-    $job = Job::find(($id));
+    $job = Job::find($id);
 
     return view('jobs.show', ['job' => $job]);
 });
 
+// Store
 Route::post('/jobs', function (){
     // ...validation
     request()->validate([
@@ -36,6 +40,48 @@ Route::post('/jobs', function (){
         'salary' => request('salary'),
         'employer_id' => 1,
     ]);
+
+    return redirect('/jobs');
+});
+
+// patch = update resource, delete = delete resource
+
+// Edit
+Route::get('/jobs/{id}/edit', function ($id){
+    $job = Job::find($id);
+
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// Update
+Route::patch('/jobs/{id}', function ($id){
+    // validate
+    request()->validate([
+        'title' => ['required', 'min:3'],
+        'salary' => ['required'],
+    ]);
+
+    // authorize
+
+    // update the job
+    // Laravel Route Model Binding
+    $job = Job::findOrFail($id);
+
+    $job->update([
+        'title' => request('title'),
+        'salary' => request('salary'),
+    ]);
+
+    return redirect('/jobs/' . $job->id);
+});
+
+// Destroy
+Route::delete('/jobs/{id}', function ($id){
+    // authorize
+    
+    // delete
+    $job = Job::findOrFail($id);
+    $job->delete();
 
     return redirect('/jobs');
 });
