7452a1b593633c27bb5d0d7a7a2b41e51ea6abac
Stephen <smatheny@ncscredit.com>
2024-05-28 17:48:41 -0500

    creating the login and registration forms and routes

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,17 @@
 <?php
 
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
 Route::resource('jobs', JobController::class);
 Route::view('/contact', 'contact');
 
-// NOTES:
-// helpful command: php artisan route:list --except-vendor
+// Auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
 
-// EXAMPLE: for ONLY specific routes
-// Route::resource('jobs', JobController::class, [
-//     'only' => ['edit']
-// ]);
-
-// EXAMPLE: for EXCEPT specific routes
-// Route::resource('jobs', JobController::class, [
-//     'except' => ['edit']
-// ]);
-
-// Essentially the same thing as above ^^^
-// Route::controller(JobController::class)->group(function () {
-//     Route::get('/jobs', 'index');
-//     Route::get('/jobs/create', 'create');
-//     Route::get('/jobs/{job}', 'show');
-//     Route::post('/jobs',  'store');
-//     Route::get('/jobs/{job}/edit',  'edit');
-//     Route::patch('/jobs/{job}',  'update');
-//     Route::delete('/jobs/{job}',  'destroy');
-// });
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
