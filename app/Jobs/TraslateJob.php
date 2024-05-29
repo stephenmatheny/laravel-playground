81537126fa9bd1b94e7510ce6bc7cecf179531f8
Stephen <smatheny@ncscredit.com>
2024-05-28 21:37:35 -0500

    mail worker and queue

---
+++ app/Jobs/TraslateJob.php
@@ -0,0 +1,32 @@
+<?php
+// NOTE: ALWAYS restart your "worker" after making a code change!!
+
+namespace App\Jobs;
+
+use App\Models\Job;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class TraslateJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(public Job $jobListing)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        logger('Translating ' . $this->jobListing->title . ' to Spanish');
+    }
+}
